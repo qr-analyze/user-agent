d0e4dc92cac3625b0b3d21fbb01fa1fcf14ba083
qr-analyze <ameliiiaphilips@gmail.com>
2024-06-08 18:40:33 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
 if (strpos($userAgent, 'iphone') !== false || strpos($userAgent, 'ipad') !== false) {
     header('Location: https://apps.apple.com/sa/app/maheer-customer/id6499317335');
@@ -8,3 +9,18 @@ if (strpos($userAgent, 'iphone') !== false || strpos($userAgent, 'ipad') !== fal
 } else {
     echo "Unsupported device";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qr-Analyze</title>
+</head>
+<body>
+    <h1>Redirecting...</h1>
+  
+</body>
+
+</html>
