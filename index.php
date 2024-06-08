05f3168d6416b9d1093e876ab04ea00d9f85642f
qr-analyze <ameliiiaphilips@gmail.com>
2024-06-08 18:40:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
 if (strpos($userAgent, 'iphone') !== false || strpos($userAgent, 'ipad') !== false) {
     header('Location: https://apps.apple.com/sa/app/maheer-customer/id6499317335');
-    exit; // Stop further execution
+    exit;
 } elseif (strpos($userAgent, 'android') !== false) {
     header('Location: https://play.google.com/store/apps/details?id=com.maheer.customerApp');
     exit; 
